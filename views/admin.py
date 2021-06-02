82b3ebce91c13c4458d9954ed39fe2e6cdb885bb
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-06-02 22:43:27 +0200

    Added first pages with javascript connection to the API

--- views/admin.py
+++ views/admin.py
@@ -4,25 +4,17 @@ from modules.response.response import Response
 from modules.app import app
 
 
-@app.route(route='/cambia-orari', methods=[Methods.GET], auth=True)
-def cambia_orari(request: Request):
+@app.route(route='/pubblica-esito', methods=[Methods.GET], auth=True)
+def pubblica_esito(request: Request):
     response = Response(request.connection)
     response.status_code(200)
-    response.file('pages/admin/cambia_orari.html')
+    response.file('pages/admin/pubblica_esito.html')
     response.send()
 
 
-@app.route(route='/elenco-dipendenti', methods=[Methods.GET], auth=True)
-def elenco_dipendenti(request: Request):
+@app.route(route='/aggiungi-contatto', methods=[Methods.GET], auth=True)
+def aggiungi_contatto(request: Request):
     response = Response(request.connection)
     response.status_code(200)
-    response.file('pages/admin/elenco_dipendenti.html')
-    response.send()
-
-
-@app.route(route='/dashboard', methods=[Methods.GET], auth=True)
-def dashboard(request: Request):
-    response = Response(request.connection)
-    response.status_code(200)
-    response.file('pages/admin/dashboard.html')
+    response.file('pages/admin/aggiungi_contatto.html')
     response.send()
